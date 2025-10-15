116e2b6e3d8b12a850e820ab65168a9131ec62d1
JoKsHuA2019 <awesomejoshua2009@gmail.com>
2025-10-14 19:03:00 -0700

    made Rocket_Functions.py more accurate to an actual rocket, added some object definitions to start implementing a PID controller

--- MainCode/Rocket_Control.py
+++ MainCode/Rocket_Control.py
@@ -1,4 +1,10 @@
 from simple_pid import PID
+import Rocket_Functions
 
 pid = PID(1, 0.1, 0.05, setpoint=0)
 
+#rocket intializer
+rocket_weight = 0.550 * 9.81 #kg * gravity = weight
+rocket_drag = 0.75 #drag coefficient
+rocket_total_liftoff_force = 80 #motor force
+myRocket = Rocket_Functions()
--- MainCode/Rocket_Functions.py
+++ MainCode/Rocket_Functions.py
@@ -1,25 +1,44 @@
+import math
+
 class Rocket:
     weight = 0 #in Newtons
+    altitude = 0 #in feet
+    cross_area = 0 #in m^2
     drag = 0 #in Newtons
     liftoff_force = 0 #in Newtons
+    velocity_up = 0 # in m/s
+    acceleration_up = 0 #in m/s^2
     x_angle = 0 #in radians
     y_angle = 0 #in radians
+    time = 0 #in seconds
+    drag_cofficient = 0 #unitless
+    air_density = 1.225 #kg/m^3 standard air density at sea level
 
-    def __init__(self, w, d, l_force):
-        weight = w
-        drag = d
-        liftoff_force = l_force
+    def __init__(self, w, d, l_force, cross, drag_cf):
+        self.weight = w
+        self.drag = d
+        self.liftoff_force = l_force
+        self.cross_area = cross
+        self.drag_coefficient = drag_cf
 
-    def adjustDirection(x, y):
+    def adjustDirection(self, x, y):
         #run servos
         #run more servos
-        x_angle += x #simulate the effect of servos moving
-        y_angle += y #in reality would be replaced with imu readings
+        self.x_angle += x #simulate the effect of servos moving
+        self.y_angle += y #in reality would be replaced with imu readings
 
-    def getX():
-        return x_angle
+    def updateState(self, x, y, s, t, al, a): #input x adjustment, y adjustment, upward speed, time, altitude, acceleration (by motors)
+        self.adjustDirection(x, y)
+        self.velocity_up = s
+        self.time = t
+        self.altitude = al
+        self.acceleration_up = a
+        totalVelocity = math.sqrt((self.velocity_up*math.tan(x))**2 + (self.velocity_up*math.tan(y))**2 + self.velocity_up**2) #full velocity, accounting for possible x & y differences
+        self.drag = 0.5 * self.drag_coefficient * self.air_density * self.cross_area * totalVelocity**2 #drag force = 0.5 * drag coefficient * density of air * cross-sectional area * total velocity^2
+
+    def getX(self):
+        return self.x_angle
     
-    def getY():
-        return y_angle
-    
+    def getY(self):
+        return self.y_angle
     
